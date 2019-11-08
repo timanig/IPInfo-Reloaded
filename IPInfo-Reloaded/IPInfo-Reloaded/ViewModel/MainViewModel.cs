eee349a66aa01b88e77ea792f187356c828c21e1
Timani G <timanig@gmail.com>
2019-11-08 17:21:18 -0500

    / Zip Code Get
    * Get your zipcode *
    ---------------------
    **This code gets your computer's info and displays the postal code**

---
+++ IPInfo-Reloaded/IPInfo-Reloaded/ViewModel/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace IPInfoReloaded
+{
+    public class MainViewModel
+    {
+        public string ZipCode { get; set; }
+        public MainViewModel()
+        {
+            IPInfoPopulate();
+        }
+        public void IPInfoPopulate()
+        {
+
+            //create client
+            var client = new HttpClient();
+            // get
+            var json = client.GetStringAsync("https://ipinfo.io/json").Result;
+            var result = JsonConvert.DeserializeObject<IPInfoModel>(json);
+            ZipCode = result.postal;
+        }
+    }
+}
---
+++ IPInfo-Reloaded/IPInfo-Reloaded/Views/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace IPInfoReloaded
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+
+          var vm = new MainViewModel();
+
+
+            Console.WriteLine($"ZipCode is: { vm.ZipCode}");
+        }
+    }
+}
